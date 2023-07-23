7763b1f6fe49d6385795c9d4f898391279a4ec2a
Vitaly Chuzhinov <vitaly.chuzhinov@noveogroup.com>
2023-07-23 20:19:41 +0300

    implement proof logic using MerkleTree

--- client/index.js
+++ client/index.js
@@ -3,13 +3,13 @@ const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
+const merkleTree = new MerkleTree(niceList);
+const name = 'Sidney Kertzmann';
 
-async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
-
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
-  });
+async function main () {
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
+  const { data: gift } = await axios.post(`${serverUrl}/gift`, { name, proof });
 
   console.log({ gift });
 }
